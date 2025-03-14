befc82f35cc08afd7cb9606fa1dbd0a87958b1c6
윤제영 <p.jyjy301@kalmate.net>
2025-03-14 14:15:29 +0900

    router 적용 commit

--- chapter3/src/components/Buttons/custom-button.jsx
+++ chapter3/src/components/Buttons/custom-button.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+const StyledTag1 = styled.button`
+  &:hover {
+    text-decoration: underline;
+  }
+  background-color: ${(props) => props.color || "green"};
+`;
+
 const CustomButton = () => {
-  return <button>커스텀 버튼</button>;
+  return (
+    <>
+      <StyledTag1 color={"purple"}>커스텀 버튼</StyledTag1>;
+      <StyledTag1>커스텀 버튼2</StyledTag1>
+    </>
+  );
 };
 
 export default CustomButton;
---
+++ chapter3/src/pages/movies.jsx
@@ -0,0 +1,5 @@
+const MoviesPage = () => {
+  return <h1>movie page</h1>;
+};
+
+export default MoviesPage;
