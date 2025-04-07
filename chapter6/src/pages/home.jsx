d7e4eb0cc3e7f5658e57184520a64f9cf8ab6e2e
=윤제영 <goods0103@naver.com>
2025-04-08 00:11:27 +0900

    메인 페이지 clear

--- chapter6/src/pages/home.jsx
+++ chapter6/src/pages/home.jsx
@@ -1,27 +1,7 @@
-import axios from "axios";
 const HomePage = () => {
-  const userData = {
-    email: "d11121@gmail.com",
-    password: "hihi",
-    passwordCheck: "hihi",
-  };
-
-  const handleSubmit = async () => {
-    try {
-      const res = await axios.post(
-        "http://localhost:3000/auth/register",
-        userData
-      );
-      console.log(res.data);
-    } catch (err) {
-      console.error("에러 발생:", err.response?.data || err.message);
-    }
-  };
-
   return (
     <>
       <h1 color="red">홈 페이지1</h1>
-      <button onClick={() => handleSubmit()}>axios 요청</button>
     </>
   );
 };
